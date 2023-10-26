1767bb5a72fa367eeb322e165104b92bf04b6781
Dima-Chuychenko <d.m.chuichenko@student.khai.edu>
2023-10-26 10:13:45 +0300

    2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -11,9 +11,12 @@ public class ChatBot {
     static void NameBotAndBD(Scanner scanner) {
         String bot_name = "Bot";
         int birth_year = 2023;
+        String user_name;
         System.out.println("Hello! My name is " + bot_name + ".");
         System.out.println("I was created in " + birth_year + ".");
-
+        System.out.println("Please, remind me your name.");
+        user_name = scanner.nextLine();
+        System.out.println("Nice to meet you, " + user_name + "!");
     }
 }
 
