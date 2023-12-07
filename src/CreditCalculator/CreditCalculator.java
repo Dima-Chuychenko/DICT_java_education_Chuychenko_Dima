4e6f36ad518b776d6c1f993d82593cae43447ce6
Dima-Chuychenko <d.m.chuichenko@student.khai.edu>
2023-12-07 11:04:38 +0200

    2-nd stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -6,23 +6,30 @@ public class CreditCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Loan principal: ");
+        // Введення основної суми кредиту
+        System.out.println("Enter the loan principal:");
         int principal = scanner.nextInt();
 
-        System.out.print("Number of months: ");
-        int months = scanner.nextInt();
+        // Вибір, що розраховувати
+        System.out.println("What do you want to calculate?");
+        System.out.println("type \"m\" - for number of monthly payments,");
+        System.out.println("type \"p\" - for the monthly payment:");
+        String choice = scanner.next();
 
-        System.out.print("Monthly payment: ");
-        int monthlyPayment = scanner.nextInt();
-
-        for (int month = 1; month <= months; month++) {
-            System.out.println("Month " + month + ": repaid " + monthlyPayment);
-            principal -= monthlyPayment;
-
-            if (principal <= 0) {
-                System.out.println("The loan has been repaid!");
-                break;
-            }
+        if (choice.equals("m")) {
+            System.out.println("Enter the monthly payment:");
+            int monthlyPayment = scanner.nextInt();
+            double months = (double) principal / monthlyPayment;
+            int roundMonths = (int) Math.round(months);
+            System.out.println("It will take " + roundMonths + " months to repay the loan");
+        } else if (choice.equals("p")) {
+            System.out.println("Enter the number of months:");
+            int months = scanner.nextInt();
+            double monthlyPayment = Math.ceil((double) principal / months);
+            int lastPayment = principal - (months - 1) * (int) monthlyPayment;
+            String result = lastPayment == (int) monthlyPayment ? "Your monthly payment = " + (int) monthlyPayment
+                    : "Your monthly payment = " + (int) monthlyPayment + " and the last payment = " + lastPayment;
+            System.out.println(result);
         }
     }
 }
