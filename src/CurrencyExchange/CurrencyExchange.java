05f4e2bcb4c18ee2e4bc548edda24737ff6272e9
Dima-Chuychenko <d.m.chuichenko@student.khai.edu>
2023-12-21 09:58:07 +0200

    2-st stage

--- src/CurrencyExchange/CurrencyExchange.java
+++ src/CurrencyExchange/CurrencyExchange.java
@@ -1,19 +1,31 @@
 package CurrencyExchange;
 
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CurrencyExchange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please, enter the number of mycoins you have: ");
+        System.out.print("Please, enter the number of coins you have: ");
         double myCoins = scanner.nextDouble();
 
-        System.out.print("Please, enter the exchange rate: ");
-        double exchangeRate = scanner.nextDouble();
+        // Курси обміну для різних валют
+        Map<String, Double> exchangeRates = new HashMap<>();
+        exchangeRates.put("ARS", 0.82);
+        exchangeRates.put("HNL", 0.17);
+        exchangeRates.put("AUD", 1.9622);
+        exchangeRates.put("MAD", 0.208);
 
-        double dollars = myCoins * exchangeRate;
+        DecimalFormat df = new DecimalFormat("#.##");
 
-        System.out.println("The total amount of dollars: " + dollars);
+        for (Map.Entry<String, Double> entry : exchangeRates.entrySet()) {
+            String currency = entry.getKey();
+            double rate = entry.getValue();
+            double result = myCoins * rate;
+            System.out.println("I will get " + df.format(result) + " " + currency + " from the sale of " + df.format(myCoins) + " mycoins.");
+        }
     }
 }
